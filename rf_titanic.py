59187680142627c5787d7b5dc8688198af318c00
theod07 <abc@def.com>
2016-02-18 15:03:01 -0800

    change fill_nan_age from age_mode to age_median

--- rf_titanic.py
+++ rf_titanic.py
@@ -29,7 +29,7 @@ train_df.Parch.unique()                         # array([0, 1, 2, 5, 3, 4, 6])
 train_df['Fare'].isnull().sum()                 # 0
 train_df[train_df['Cabin'].isnull()].count()    # 687 may be able to map cabin to fare
 train_df[train_df['Age'].isnull()].count()      # 177
-train_df[train_df['Embarked']isnull()].count()  # 2
+train_df[train_df['Embarked'].isnull()].count()  # 2
 
 train_df['Embarked'].unique()                   # ['S', 'C', 'Q', nan]
 train_df[train_df['Embarked'] == 'S'].count()   # 644
@@ -38,13 +38,20 @@ train_df[train_df['Embarked'] == 'Q'].count()   # 77
 
 embarked_mode = train_df['Embarked'].mode()[0]
 train_df['Embarked'].fillna(embarked_mode, inplace=True)
-age_mode = train_df.Age.mode()[0]
-train_df['Age'].fillna(age_mode, inplace=True)
+age_mode = train_df.Age.mode().values
+age_median = train_df.Age.dropna().median()
+# train_df['Age'].fillna(age_mode, inplace=True)
+train_df['Age'].fillna(age_median, inplace=True)
 
 train_df['embarked_cat'] = train_df['Embarked'].map( {'S': 1, 'C': 2, 'Q':3} ).astype(int)
 
 # model features: Pclass, Gender, Age, embarked_cat
+# fill NaN age with age_mode
 # results: 0.57895
+
+# model features: Pclass, Gender, Age, embarked_cat
+# fill Nan age with age_median
+# results:
 
 
 X_train = train_df[['Pclass', 'Gender', 'Age', 'embarked_cat']]
@@ -56,9 +63,10 @@ rf.fit(X_train, y_train)
 test_df = pd.read_csv('./data/test.csv')
 test_df['Gender'] = train_df['Sex'].map( {'female': 0, 'male': 1} ).astype(int)
 test_df['Embarked'].fillna(embarked_mode, inplace=True)
-test_df['Age'].fillna(age_mode, inplace=True)
+# test_df['Age'].fillna(age_mode, inplace=True)
+test_df['Age'].fillna(age_median, inplace=True)
 test_df['embarked_cat'] = train_df['Embarked'].map( {'S': 1, 'C': 2, 'Q':3} ).astype(int)
 X_test = test_df[['Pclass', 'Gender', 'Age', 'embarked_cat']]
 
 test_df['Survived'] = rf.predict(X_test)
-test_df.to_csv('prediction.csv', index_label)
+test_df.to_csv('prediction.csv', index_label=False, index=False, columns=['PassengerId', 'Survived'])
